b76f4709017196774e1621b3dfc8afc5c5bb07ee
Alberto Abreu <abreualberto91@gmail.com>
2019-07-04 22:15:50 +0200

    Adding PKs and FKs to the tables

--- your_code/creating_seeding_tables.sql
+++ your_code/creating_seeding_tables.sql
@@ -1,20 +1,32 @@
 -- GETTING TO KNOW OUR DATA.
 
-
--- Creating the province table with the re_id from the spanish political table
+-- Creating province table
 CREATE TABLE province (
-	re_id INT,
+	prov_id INT,
 	prov_name VARCHAR(30),
-	PRIMARY KEY (re_id)
+	PRIMARY KEY (prov_id)
 );
 
 -- Inserting data into our province table
-INSERT INTO province (re_id, prov_name)
+INSERT INTO province (prov_id, prov_name)
 VALUES 
 (8,'Barcelona'),
 (25,'Lleida'),
 (43,'Girona'),
 (17,'Tarragona');
+
+-- Adding the primary key
+ALTER TABLE MUN_ID_TABLE
+ADD PRIMARY KEY(muni_id);
+
+-- Adding the foreign key
+ALTER TABLE MUN_ID_TABLE
+ADD FOREIGN KEY(prov_id) REFERENCES province(prov_id);
+
+-- Adding foreign keys
+ALTER TABLE MUN_POL_AGE
+ADD FOREIGN KEY(muni_id) REFERENCES MUN_ID_TABLE(muni_id),
+ADD FOREIGN KEY(prov_id) REFERENCES province(prov_id);
 
 
 
